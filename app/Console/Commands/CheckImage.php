4e0ca8e795720414cde30a5563b9a1e5a46eb04c
Samsul Anwar <sam.aan46@gmail.com>
2026-05-11 22:33:57 +0700

    fixed download image from erp mpm

--- app/Console/Commands/CheckImage.php
+++ app/Console/Commands/CheckImage.php
@@ -49,14 +49,15 @@ class CheckImage extends Command
                 // jika tidak ada, download ulang image
                 $this->info('Product: '.$row->kodeprod.' - '.$row->image.' not found');
                 try {
-                    $url = $row->image_backup;
-                    $info = pathinfo($url);
-                    $contents = file_get_contents($url);
-                    $rel_path = '/images/product/';
-                    $new_name = $row->kodeprod . "." . $info['extension'];
-                    $product_image = $rel_path . $new_name;
-                    $image_resize = InterImage::make($contents);
-                    $image_resize->save(('public/images/product/' . $new_name));
+                    // $url = $row->image_backup;
+                    // $info = pathinfo($url);
+                    // $contents = file_get_contents($url);
+                    // $rel_path = '/images/product/';
+                    // $new_name = $row->kodeprod . "." . $info['extension'];
+                    // $product_image = $rel_path . $new_name;
+                    // $image_resize = InterImage::make($contents);
+                    // $image_resize->save(('public/images/product/' . $new_name));
+                    $this->download($row);
                 } catch(\Exception $e) {
                     $this->info($e->getMessage());
                 }
@@ -78,6 +79,70 @@ class CheckImage extends Command
         }
     }
 
+    public function download($row)
+    {
+        $url = $row->image_backup;
+        $info = pathinfo($url);
+        $new_name = $row->kodeprod . "." . $info['extension'];
+
+        $ch = curl_init($url);
+
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_TIMEOUT => 60,
+            CURLOPT_CONNECTTIMEOUT => 20,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_USERAGENT => 'Mozilla/5.0'
+        ]);
+
+        $contents = curl_exec($ch);
+
+        $error = curl_error($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+        curl_close($ch);
+
+        if ($error) {
+            throw new \Exception($error);
+        }
+
+        if ($httpCode != 200) {
+            throw new \Exception('HTTP Error: '.$url.' Code: '. $httpCode);
+        }
+
+        if (!str_contains($contentType, 'image')) {
+            throw new \Exception('Response bukan image');
+        }
+
+        $image_resize = InterImage::make($contents);
+
+        $image_resize->resize(800, null, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        $image_resize->save(
+            public_path('images/product/' . $new_name),
+            80
+        );
+    }
+
+    // notif wa
+    public function sendWaGroup($msg)
+    {
+        $link = config('app.url');
+
+        Http::withHeaders([
+            'x-api-key' => config('wabot.x_api_key')
+        ])->post(config('wabot.url').'/send-group', [
+            'groupId' => config('wabot.group_id'),
+            'message' =>  $link."\n".$msg
+        ])->json();
+    }
+
     /**
      * Execute the console command.
      *
@@ -85,7 +150,9 @@ class CheckImage extends Command
      */
     public function handle()
     {
+        $this->sendWaGroup('Check Image sedang di proses');
         $this->check();
+        $this->sendWaGroup('Check Image sukses di proses');
         $this->info('Finish The commands.');
     }
 }
